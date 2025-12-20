68604124ba48a3db386202bd1cd0fb3652a5b70b
Jimmie Johansson <jimmitjoo@gmail.com>
2025-12-20 20:02:50 +0100

    middleware: use g.Config.Cookie instead of g.config.cookie

--- middleware.go
+++ middleware.go
@@ -2,7 +2,6 @@ package gemquick
 
 import (
 	"net/http"
-	"strconv"
 
 	"github.com/justinas/nosurf"
 )
@@ -14,7 +13,6 @@ func (g *Gemquick) SessionLoad(next http.Handler) http.Handler {
 
 func (g *Gemquick) NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
-	secure, _ := strconv.ParseBool(g.config.cookie.secure)
 
 	// Exempt API from CSRF protection:
 	csrfHandler.ExemptGlob("/api/*")
@@ -22,9 +20,9 @@ func (g *Gemquick) NoSurf(next http.Handler) http.Handler {
 	csrfHandler.SetBaseCookie(http.Cookie{
 		HttpOnly: true,
 		Path:     "/",
-		Secure:   secure,
+		Secure:   g.Config.Cookie.Secure,
 		SameSite: http.SameSiteStrictMode,
-		Domain:   g.config.cookie.domain,
+		Domain:   g.Config.Cookie.Domain,
 	})
 
 	return csrfHandler
