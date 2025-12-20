5952a0a7f72c47c3bdfdeb9627ef1254dcd3c2ec
Jimmie Johansson <jimmitjoo@gmail.com>
2025-12-20 20:02:42 +0100

    types: remove unused cookieConfig, databaseConfig, redisConfig

--- types.go
+++ types.go
@@ -4,25 +4,3 @@ type initPaths struct {
 	rootPath    string
 	folderNames []string
 }
-
-type cookieConfig struct {
-	name     string
-	lifetime string
-	persist  string
-	secure   string
-	domain   string
-}
-
-type databaseConfig struct {
-	dsn      string
-	database string
-}
-
-// Database is now defined in services.go
-
-type redisConfig struct {
-	host     string
-	port     string
-	password string
-	prefix   string
-}
